92dbb6a0bc740e4130cb9ea3486bb7ab8191e8be
sunshine <3105461516@qq.com>
2026-09-04 15:27:28 +0800

    feat: add social links card to about page
    - SocialLinks widget: GitHub / Douyin / Bilibili / Email / Steam
    - icons: fa6-brands:github|bilibili|steam, fa6-solid:envelope,
      material-symbols:smart-display-rounded (douyin), arrow-outward
    - astro.config: declare material-symbols icon set (replace stray key)
    - order on about: content card -> social links -> word heatmap

--- astro.config.mjs
+++ astro.config.mjs
@@ -49,7 +49,7 @@ export default defineConfig({
 		}),
 		icon({
 			include: {
-				"preprocess: vitePreprocess(),": ["*"],
+				"material-symbols": ["*"],
 				"fa6-brands": ["*"],
 				"fa6-regular": ["*"],
 				"fa6-solid": ["*"],
